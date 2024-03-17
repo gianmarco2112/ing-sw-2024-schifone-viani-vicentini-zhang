40a93124b947cd6fcd140271c216457a9e0125e7
alviabea <alessiabeatrice.viani@mail.polimi.it>
2024-03-17 18:41:59 +0100

    Comments for the enumerations "Color", "Symbol" and for the abstract class "PlayerAreaCard" added

--- src/main/java/ingsw/codex_naturalis/model/enumerations/Color.java
+++ src/main/java/ingsw/codex_naturalis/model/enumerations/Color.java
@@ -1,5 +1,9 @@
 package ingsw.codex_naturalis.model.enumerations;
-
+/**
+ * Color's enumeration
+ * Each player is represented on the score track by a token of a specific color.
+ * This enumeration represents the 4 possibles colors that a player could choose.
+ */
 public enum Color {
     RED,
     BLUE,

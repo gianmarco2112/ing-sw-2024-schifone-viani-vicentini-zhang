018d4134c86ce1be34f684aa4a7e230187033c9f
andreazhang <andrea.zhang@polimi.mail.com>
2024-03-30 16:18:19 +0100

    Player Test

--- src/test/java/ingsw/codex_naturalis/model/PlayerTest.java
+++ src/test/java/ingsw/codex_naturalis/model/PlayerTest.java
@@ -1,10 +1,13 @@
 package ingsw.codex_naturalis.model;
 
-import ingsw.codex_naturalis.exceptions.NicknameAlreadyExistsException;
+import ingsw.codex_naturalis.exceptions.NotPlayableException;
 import ingsw.codex_naturalis.model.cards.HandPlayableCard;
+import ingsw.codex_naturalis.model.cards.initial.InitialCard;
+import ingsw.codex_naturalis.model.cards.initial.InitialCardBack;
+import ingsw.codex_naturalis.model.cards.initial.InitialCardFront;
 import ingsw.codex_naturalis.model.enumerations.Color;
 import ingsw.codex_naturalis.model.enumerations.Symbol;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,30 +15,69 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Player player;
+    CenterOfTable centerOfTable;
+    InitialCard initialCard;
+    @BeforeEach
+    void setUp(){
+        player = new Player("Test",Color.RED,1);
+        centerOfTable = new CenterOfTable();
+        player.setCenterOfTable(centerOfTable);
+        initialCard = new InitialCard(new InitialCardFront(Symbol.EMPTY,new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false)),
+                                                  new InitialCardBack(Symbol.EMPTY, new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false),
+                                                                                    new Corner(Symbol.EMPTY,false),
+                                                                                    List.of(Symbol.EMPTY,Symbol.EMPTY,Symbol.EMPTY)));
+        player.setInitialCard(initialCard);
+        player.drawFromResourceCardsDeck();
+        player.drawFromResourceCardsDeck();
+        player.drawFromGoldCardsDeck();
+    }
 
     @Test
-    void nickname(){
-        Player player = new Player("Bob", Color.RED,1);
-        assertEquals("Bob",player.getNickname());
+    void testPlayerProperties(){
+        assertEquals("Test",player.getNickname());
         assertEquals(Color.RED,player.getColor());
-        Player player1 = new Player("Bob",Color.BLUE,2);
-        assertEquals("Bob",player.getNickname());
-        assertEquals(Color.RED,player.getColor());
-        Game game = new Game(1);
-        try {
-            game.addPlayer(player);
-            game.addPlayer(player1);
-        }catch (NicknameAlreadyExistsException e){
-            System.out.println(e.getMessage());
-        }
-
-
+        assertEquals(centerOfTable,player.getCenterOfTable());
+        assertEquals(initialCard,player.getInitialCard());
+        assertEquals(1,player.getPlayerID());
+        assertEquals(0,player.getSentMessages().size());
     }
     @Test
-    void playCard() {
+    void playInitialCard(){
+        player.playInitialCard();
+        PlayerArea playerArea = player.getPlayerArea();
+
+        assertEquals(initialCard.getBack(),playerArea.getCardOnCoordinates(0,0));
+    }
+    @Test
+    void testNotPlayableException() {
+        playInitialCard();
+        Exception exception;
+        Exception exception1;
+        //coordinate non valide
+        exception = assertThrows(NotPlayableException.class,() -> player.playCard(player.getHand().getFirst(),10,10 ));
+        assertEquals("This card is not playable here!",exception.getMessage());
+        //non risorse sufficienti per poter piazzare la carta
+        exception1 = assertThrows(NotPlayableException.class,() -> player.playCard(player.getHand().get(2),1,1 ));
+        assertEquals("This card is not playable here!",exception1.getMessage());
+
     }
 
     @Test
+    void testPlayCard(){
+        playInitialCard();
+        HandPlayableCard playableSide = player.getHand().getFirst();
+        player.playCard(player.getHand().getFirst(),1,1);//attenzione, dopo aver giocato la carta essa viene rimossa dalla mano
+        assertEquals(playableSide.getHandPlayableFront(),
+                     player.getPlayerArea().getCardOnCoordinates(1,1));
+    }
+
+    /*@Test
     void drawFromResourceCardsDeck() {
         Player player = new Player("Bob", Color.RED,1);
         Player player1 = new Player("Bob",Color.RED,2);
@@ -44,7 +86,7 @@ class PlayerTest {
         Game game = new Game(1);
         game.addPlayer(player);
         game.dealInitialCard();
-        /*List<HandPlayableCard> hand = player.getList();
+        *//*List<HandPlayableCard> hand = player.getList();
         assertTrue(hand.isEmpty());
         player.playInitialCard();
 
@@ -53,6 +95,6 @@ class PlayerTest {
         player.drawFromResourceCardsDeck();
         assertFalse(hand.isEmpty());
         player.playCard(hand.getFirst(), 0, 0);
-        assertTrue(hand.isEmpty());*/
-    }
+        assertTrue(hand.isEmpty());*//*
+    }*/
 }

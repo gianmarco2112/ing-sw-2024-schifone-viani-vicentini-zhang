932850cb2e570bb03068734f39c7496e8ec5dceb
andreazhang <andrea.zhang@polimi.mail.com>
2024-03-30 13:22:41 +0100

    add a class which contains all default values

---
+++ src/main/java/ingsw/codex_naturalis/model/DefaultValue.java
@@ -0,0 +1,10 @@
+package ingsw.codex_naturalis.model;
+
+public class DefaultValue {
+    public final static int maxNumOfPlayer = 4;
+    public final static int minNumOfPlayer = 2;
+    public final static int totNumOfResourceCards = 40;
+    public final static int totNumOfGoldCards = 40;
+    public final static int totNumOfObjectiveCards = 16;
+    public final static int totNumOfInitialCards = 6;
+}

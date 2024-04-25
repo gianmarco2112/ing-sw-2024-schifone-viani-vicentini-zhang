a8bb14dbbf546a2db4e81931fac87f9a59e75005
andreazhang <andrea.zhang@polimi.mail.com>
2024-04-25 09:37:11 +0200

    changed GameplayGraphicUI class' signature, it extends Gameplay

--- src/main/java/ingsw/codex_naturalis/view/gameplayPhase/GameplayGraphicUI.java
+++ src/main/java/ingsw/codex_naturalis/view/gameplayPhase/GameplayGraphicUI.java
@@ -1,35 +1,10 @@
 package ingsw.codex_naturalis.view.gameplayPhase;
 
-import ingsw.codex_naturalis.controller.gameplayPhase.GameplayObserver;
-import ingsw.codex_naturalis.events.gameplayPhase.DrawCard;
-import ingsw.codex_naturalis.events.gameplayPhase.FlipCard;
-import ingsw.codex_naturalis.events.gameplayPhase.Message;
-import ingsw.codex_naturalis.events.gameplayPhase.PlayCard;
-import ingsw.codex_naturalis.exceptions.NotYourDrawTurnStatusException;
-import ingsw.codex_naturalis.exceptions.NotYourTurnException;
-
 import javax.swing.*;
-import java.util.List;
 
-public class GameplayGraphicUI extends JFrame implements GameplayObserver {
-
+public class GameplayGraphicUI extends GameplayUI{
     @Override
-    public void updateFlipCard(FlipCard flipCard) {
-
-    }
-
-    @Override
-    public void updatePlayCard(PlayCard playCard, int x, int y) throws NotYourTurnException {
-
-    }
-
-    @Override
-    public void updateDrawCard(DrawCard drawCard) throws NotYourTurnException, NotYourDrawTurnStatusException {
-
-    }
-
-    @Override
-    public void updateText(Message message, String content, List<String> receivers) {
+    public void run() {
 
     }
 }

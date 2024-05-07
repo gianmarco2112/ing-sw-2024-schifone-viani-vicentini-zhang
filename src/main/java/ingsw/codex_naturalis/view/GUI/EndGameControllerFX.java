cc6fe1d5bc55b437080fea548aafa53c8b6ef345
andreazhang <andrea.zhang@polimi.mail.com>
2024-05-07 11:17:02 +0200

    added resources for login and lobby phase. implemented partially gui for login and lobby phase, not connected to mvc and network yet.

---
+++ src/main/java/ingsw/codex_naturalis/view/GUI/EndGameControllerFX.java
@@ -0,0 +1,8 @@
+package ingsw.codex_naturalis.view.GUI;
+
+import ingsw.codex_naturalis.server.model.Game;
+
+public class EndGameControllerFX {
+    public void createRankingTable(Game.Immutable msg) {
+    }
+}

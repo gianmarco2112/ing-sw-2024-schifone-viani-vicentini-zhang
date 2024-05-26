5295f563f4b27164de644a413daee635b7163eee
alviabea <alessiabeatrice.viani@mail.polimi.it>
2024-05-26 19:15:46 +0200

    Modified "PlayerTest" class to improve coverage

--- src/test/java/ingsw/codex_naturalis/model/PlayerTest.java
+++ src/test/java/ingsw/codex_naturalis/model/PlayerTest.java
@@ -1,5 +1,7 @@
 package ingsw.codex_naturalis.model;
-
+import ingsw.codex_naturalis.server.exceptions.NotYourTurnException;
+import ingsw.codex_naturalis.server.exceptions.NotPlayableException;
+import ingsw.codex_naturalis.server.exceptions.NotYourPlayTurnStatusException;
 import ingsw.codex_naturalis.server.model.cards.Corner;
 import ingsw.codex_naturalis.server.model.cards.initialResourceGold.PlayableCard;
 import ingsw.codex_naturalis.server.model.cards.initialResourceGold.PlayableSide;
@@ -9,37 +11,45 @@ import ingsw.codex_naturalis.common.enumerations.PlayableCardType;
 import ingsw.codex_naturalis.common.enumerations.Symbol;
 import ingsw.codex_naturalis.common.enumerations.TurnStatus;
 import ingsw.codex_naturalis.server.model.player.Player;
+import ingsw.codex_naturalis.server.model.cards.objective.ObjectiveCard;
+import ingsw.codex_naturalis.server.model.cards.objective.PatternObjectiveCard;
+import ingsw.codex_naturalis.server.model.cards.objective.SymbolsObjectiveCard;
+import ingsw.codex_naturalis.server.model.player.PlayerArea;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import ingsw.codex_naturalis.server.model.Deck;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * it tests player's properties (nickname, color, id, initial card ect) and actions (draw, play a card)
- */
 class PlayerTest {
-    Player player = new Player("Test");
-    private PlayableCard initialCard(){
+
+    Player player = new Player("TestPlayer");
+    Player player1 = new Player("TestPlayer1");
+    private PlayableCard initialCard() {
         PlayableCard initialCard;
         initialCard = new PlayableCard(
                 "I00",
                 PlayableCardType.INITIAL,
                 Symbol.EMPTY,
                 new PlayableSide(
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false)),
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false)),
                 new Back(
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false),
-                        new Corner(Symbol.EMPTY,false),
-                        List.of(Symbol.EMPTY,Symbol.EMPTY,Symbol.EMPTY)));
-        return(initialCard);
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false),
+                        new Corner(Symbol.EMPTY, false),
+                        List.of(Symbol.EMPTY, Symbol.EMPTY, Symbol.EMPTY)));
+        return (initialCard);
     }
+
     private PlayableCard insectResourceCard(){
         PlayableCard resourceCard;
         resourceCard= new PlayableCard(
@@ -60,20 +70,156 @@ class PlayerTest {
         );
         return(resourceCard);
     }
+
+    private SymbolsObjectiveCard symbolObjectiveCard(){
+        HashMap<Symbol, Integer> symbolsMap = new HashMap<>();
+        symbolsMap.put(Symbol.QUILL, 2);
+        SymbolsObjectiveCard symbolsObjectiveCard = new SymbolsObjectiveCard(
+                "Test",
+                2,
+                symbolsMap
+        );
+        return (symbolsObjectiveCard);
+    }
+    /**
+     * Tests setColor (), getNickname (), setTurnStatus (), getTurnStatus (), getPlayerArea ()
+     */
     @Test
-    void simplePlayerTest(){
+    void testPlayerAttributes() {
+        assertNotNull(player.getPlayerArea());
+        player.setColor(Color.RED);
+        assertEquals(Color.RED, player.getColor());
+        assertEquals("TestPlayer", player.getNickname());
+        player.setTurnStatus(TurnStatus.PLAY);
+        assertEquals(TurnStatus.PLAY, player.getTurnStatus());
+        player.setTurnStatus(TurnStatus.DRAW);
+        assertEquals(TurnStatus.DRAW, player.getTurnStatus());
+    }
+    /**
+     * Tests flipInitialCard ()
+     */
+    @Test
+    void testFlipInitialCard() {
         PlayableCard initialCard = initialCard();
         player.setInitialCard(initialCard);
-        assertEquals(initialCard,player.getInitialCard());
-        player.setTurnStatus(TurnStatus.DRAW);
-        assertEquals(TurnStatus.DRAW,player.getTurnStatus());
-        player.setColor(Color.RED);
-        assertEquals(Color.RED,player.getColor());
-        assertEquals("Test",player.getNickname());
-        PlayableCard resourceCard = insectResourceCard(); //showing back initially
-        List<PlayableCard> hand = new ArrayList<>();
-        hand.add(resourceCard);
-        player.setHand(hand);
-        assertEquals(resourceCard,player.getHand().getFirst());
+        player.flipInitialCard();
+        assertTrue(initialCard.getImmutablePlayableCard().showingFront());
+        // showingFront è inizializzato a false nel costruttore di initialCard (),
+        // quindi flippandolo deve mostrare true
     }
+
+    /**
+     * Tests flipCard (int index)
+     */
+    @Test
+    void testFlipHandCard() {
+        PlayableCard card1 = insectResourceCard();
+        PlayableCard card2 = insectResourceCard();
+        List<PlayableCard> hand = new ArrayList<>();
+        hand.add(card1);
+        hand.add(card2);
+        player.setHand(hand);
+
+        player.flipCard(0);
+        assertTrue(card1.getImmutablePlayableCard().showingFront());
+
+        player.flipCard(1);
+        assertTrue(card2.getImmutablePlayableCard().showingFront());
+    }
+
+    /**
+     * Tests getInitialCard() and setInitialCard ()
+     */
+    @Test
+    void testGetInitialCard() {
+        PlayableCard initialCard = initialCard();
+        player.setInitialCard(initialCard);
+        assertEquals(initialCard, player.getInitialCard());
+    }
+
+    /**
+     * Tests playInitialCard ()
+     */
+    @Test
+    void testPlayInitialCard() {
+        PlayableCard initialCard = initialCard();
+        player.setInitialCard(initialCard);
+        player.playInitialCard();
+        assertNull(player.getInitialCard());
+        assertEquals(initialCard, player.getPlayerArea().getCardOnCoordinates(0, 0));
+    }
+
+    /**
+     * Tests playCard (), getHand (), setHand (), setupHand ()
+     */
+    @Test
+    void testPlayCard() throws NotYourTurnException, NotYourPlayTurnStatusException, NotPlayableException {
+
+        player.setTurnStatus(TurnStatus.PLAY);
+        PlayableCard card1 = insectResourceCard();
+        PlayableCard card2 = insectResourceCard();
+        PlayableCard card3 = initialCard();
+        List<PlayableCard> hand = new ArrayList<>();
+        hand.add(card1);
+        hand.add(card2);
+        player.setHand(hand);
+        assertEquals (hand, player.getHand());
+        player1.setupHand(hand);
+        assertEquals (hand, player1.getHand());
+        player.setInitialCard(card3);
+        player.playInitialCard();
+        player.playCard(0, 1, 1);
+        assertFalse(player.getHand().contains(card1));
+        assertEquals(card1, player.getPlayerArea().getCardOnCoordinates(1, 1));
+    }
+
+    /**
+     * Tests drawCard (PlayableCard playableCard)
+     */
+    @Test
+    void testDrawCard() {
+        PlayableCard card = insectResourceCard();
+        player.drawCard(card);
+        assertTrue(player.getHand().contains(card));
+    }
+
+    /**
+     * Tests getSecretObjectiveCards () and setupSecretObjectiveCards (List<ObjectiveCard> secretObjectiveCards)
+     * and chooseObjectiveCard(int index, Deck<ObjectiveCard> objectiveCardsDeck)
+     */
+    @Test
+    void testGetSecretObjectiveCards() {
+        SymbolsObjectiveCard card1 = symbolObjectiveCard();
+        SymbolsObjectiveCard card2 = symbolObjectiveCard();
+        List<ObjectiveCard> objectiveCards = new ArrayList<>();
+        objectiveCards.add(card1);
+        objectiveCards.add(card2);
+        Deck<ObjectiveCard> objectiveCardsDeck = new Deck (objectiveCards);
+        ObjectiveCard playerCard1 = objectiveCardsDeck.drawACard();
+        ObjectiveCard playerCard2 = objectiveCardsDeck.drawACard();
+        List<ObjectiveCard> secretObjectiveCards = new ArrayList<>();
+        secretObjectiveCards.add(playerCard1);
+        secretObjectiveCards.add(playerCard2);
+        player.setupSecretObjectiveCards(secretObjectiveCards);
+        assertEquals(secretObjectiveCards, player.getSecretObjectiveCards());
+        assertEquals(2, secretObjectiveCards.size());
+        assertTrue(secretObjectiveCards.contains(card1));
+        assertTrue(secretObjectiveCards.contains(card2));
+        player.chooseObjectiveCard(0, objectiveCardsDeck);
+        assertEquals(1, player.getSecretObjectiveCards().size());
+        assertEquals(card1, player.getSecretObjectiveCards().get(0));
+        assertEquals(card1, player.getPlayerArea().getObjectiveCard());
+    }
+
+    /**
+     * Tests isInGame (), setInGame (), setReady ()
+     */
+    @Test
+    void testIsInGame() {
+        assertTrue(player.isInGame());
+        player.setReady(true);
+        player.setInGame(false);
+        assertFalse(player.isInGame());
+    }
+
 }
